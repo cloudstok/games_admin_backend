b875393297fdeffcd88f2355f617783e8d6c7a67
prashantg30 <prashant.gupta@cloudstok.com>
2024-06-05 16:36:32 +0530

    operator user login with auth

--- controller/operator/user.js
+++ controller/operator/user.js
@@ -2,7 +2,7 @@ const { read, write } = require("../../db_config/db");
 
 const addUser = async (req, res) => {
     try {
-        const { name, currency_preference, profile_url } = req.body;
+        const { operator_id, name, currency_preference, profile_url } = req.body;
         const {userId, password}= generateRandomString(name, 10);
         const sql = "insert into user (name, user_id , password , profile_url, currency_prefrence) values(?, ?, ? , ?, ?)"
         await write.query(sql, [name, userId, password, profile_url,  currency_preference]);
@@ -22,6 +22,24 @@ function generateRandomString(name, length) {
     return {password,  userId: `${name}_${Math.round(Math.random() * 10000)}`};
   }
   
+
+  const userLogin = async(req, res)=> {
+    try{
+        const { userId, password} = req.body;
+        const [getUser] = write.query(`SELECT * FROM user WHERE user_id = ?`, [userId]);
+        if(getUser.length > 0){
+            const {user_id, name, profile_url, currency_preference} = getUser[0];
+            const reqTime = Date.now();
+
+        }else{
+            return res.status(400).send({ status: false, msg: "User does not exists"});
+        }
+        
+    }catch(err){
+        console.error(err);
+        return res.send(500).send({ status: false, msg: "Internal Server Error"});
+    }
+  }
 
 // const getUser = async (req, res) => {
 //     try {
