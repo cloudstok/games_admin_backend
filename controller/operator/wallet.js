9440c8bc905cd5981f19d72c7cfecada697a38e7
prashantg30 <prashant.gupta@cloudstok.com>
2024-06-11 11:49:57 +0530

    wallet balance added hard coded

--- controller/operator/wallet.js
+++ controller/operator/wallet.js
@@ -3,8 +3,8 @@ const { read, write } = require("../../db_config/db");
 const { decryption } = require("../../utilities/ecryption-decryption");
 const addWallet = async (req ,res)=>{
     try{
-        const {user_id ,balance } = req.body
-       await write.query("insert into user_wallet (user_id ,balance) value(? , ?)" , [user_id , balance])
+        const {user_id } = req.body
+       await write.query("insert into user_wallet (user_id ,balance) value(? , ?)" , [user_id , '3000'])
        return res.status(200).send({ status: true, msg: "Wallet Add successfully to master's list" })
     }catch(err){
         console.log(err)
