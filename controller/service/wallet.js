2935c3953858d6f271a38035227cb891e7dd2991
vishal <vishalkumar8673@gmail.com>
2024-07-01 13:09:30 +0530

    ok

--- controller/service/wallet.js
+++ controller/service/wallet.js
@@ -51,11 +51,7 @@ const getUserBalance = async (req, res) => {
 const updateUserBalance = async (req, res) => {
     try {
         const token = req.headers.token;
-<<<<<<< HEAD
-        const { balance, txn_id, description, txn_type } = req.body;
-=======
         const { amount , txn_id , description, txn_type } = req.body;
->>>>>>> 0af196db1b846786260fd925a2a2d6c9135af891
         let validateUser = await getRedis(token);
         try {
             validateUser = JSON.parse(validateUser);
@@ -65,13 +61,8 @@ const updateUserBalance = async (req, res) => {
         if (validateUser) {
             const { operatorId, secret, userId } = validateUser;
             const operatorUrl = await getWebhookUrl(operatorId, "UPDATE_BALANCE");
-<<<<<<< HEAD
-            let encryptedData = await encryption({ balance, txn_id, description, txn_type }, secret);
-            if (operatorUrl) {
-=======
             let encryptedData = await encryption({ amount , txn_id , description, txn_type }, secret);
             if(operatorUrl){
->>>>>>> 0af196db1b846786260fd925a2a2d6c9135af891
                 const options = {
                     method: 'POST',
                     url: operatorUrl,
@@ -87,13 +78,8 @@ const updateUserBalance = async (req, res) => {
                 await axios(options).then(async data => {
                     // userId, balance , update , operatorId, url , data
                     // history transaction 
-<<<<<<< HEAD
-                    const sql_transaction = "INSERT INTO transaction (userId, balance, operatorId, data , txn_id ,  description, txn_type) VALUES (? ,?, ?, ? ,? , ?, ?)";
-                    await write.query(sql_transaction, [userId, balance, operatorId, JSON.stringify(data?.data), txn_id, description, txn_type])
-=======
                    let sql = "INSERT INTO transaction (userId, balance, operatorId, data , txn_id ,  description, txn_type) VALUES (? ,?, ?, ? ,? , ?, ?)";
                      await write.query(sql , [ userId, amount  , operatorId, JSON.stringify(data?.data) , txn_id , description, txn_type])
->>>>>>> 0af196db1b846786260fd925a2a2d6c9135af891
                     if (data.status === 200) {
                         return res.status(200).send(data.data);
                     } else {
@@ -102,23 +88,11 @@ const updateUserBalance = async (req, res) => {
                     }
                 }).catch(async err => {
                     let data = err?.response?.data
-<<<<<<< HEAD
-                    const sql_transaction = "INSERT INTO transaction (userId, balance, operatorId, data,  txn_id ,  description, txn_type) VALUES (? ,?, ?, ? ,?,?,?)";
-
-                    const sql_rollback_detail = "INSERT INTO rollback_detail (game_url, options) VALUES (?, ?, ? , ?)"
-
-                    await write.query(sql_transaction, [userId, balance, operatorId, JSON.stringify(data?.data), txn_id, description, txn_type])
-                    await write.query(sql_rollback_detail, [req.url, options])
-                    return res.status(500).send({ status: false, msg: "Internal Server error" });
-                    console.error(`[ERR] while updating user balance from operator is::`, JSON.stringify(err))
-                    // return res.status(500).send({ status: false, msg: "We've encountered an internal error" });
-=======
                     let sql = "INSERT INTO transaction (userId, balance, operatorId, data,  txn_id ,  description, txn_type) VALUES (? ,?, ?, ? ,?,?,?)";
                     await write.query(sql , [ userId, amount  , operatorId, JSON.stringify(data?.data), txn_id , description, txn_type])
                     return res.status(500).send( {status:false , msg : "Internal Server error"} );
                    // console.error(`[ERR] while updating user balance from operator is::`, JSON.stringify(err))
                    // return res.status(500).send({ status: false, msg: "We've encountered an internal error" });
->>>>>>> 0af196db1b846786260fd925a2a2d6c9135af891
                 })
             } else {
                 return res.status(400).send({ status: false, msg: "No URL configured for the event" });
