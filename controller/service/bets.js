7fd64cc07fd24b3091cadd3c34d05dc41a7c5649
vishal <vishalkumar8673@gmail.com>
2024-07-16 18:28:40 +0530

    manual rollback

--- controller/service/bets.js
+++ controller/service/bets.js
@@ -18,7 +18,7 @@ const bets = async (req, res) => {
 
 const manualCashoutOrRollback = async (req, res) => {
     try {
-        const { id, event, operator_id } = req.body;
+        const { id, event, operator_id, description, amount, txn_ref_id, user_id, session_token } = req.body;
         const [getTransaction] = await write.query(`SELECT * FROM pending_transactions where id = ? and txn_status = '1'`, [id]);
         if (getTransaction.length > 0) {
             let { options, cashout_retries, rollback_retries, transaction_id } = getTransaction[0];
@@ -34,47 +34,55 @@ const manualCashoutOrRollback = async (req, res) => {
             let [operator] = await write.query(`SELECT * FROM operator where user_id = ?`, [operator_id]);
             if (operator.length > 0) {
                 let secret = operator[0].secret;
-                let data = options;
                 let operatorUrl = await getWebhookUrl(operator_id, "UPDATE_BALANCE");
                 if (operatorUrl) {
-                    if (event === 'cashout') {
-                        cashout_retries += 1;
-                        await write.query(`UPDATE pending_transactions SET cashout_retries = ?, event = ? WHERE id = ?`, [cashout_retries, 'cashout', id]);
-                    } else {
-                        rollback_retries += 1;
-                        await write.query(`UPDATE pending_transactions SET rollback_retries = ?, event = ? WHERE id = ?`, [rollback_retries, 'rollback', id]);
-                        let { token, txn_ref_id } = options;
-                        let [getRollbackTransaction] = await write.query(`SELECT * FROM transaction WHERE txn_id = ?`, [txn_ref_id]);
-                        if (getRollbackTransaction.length > 1) {
-                            getRollbackTransaction = getRollbackTransaction[1];
-                        } else {
-                            getRollbackTransaction = getRollbackTransaction[0];
-                            rollbackFlag = 1;
-                        }
-                        let rollbackAmount = getRollbackTransaction.amount;
-                        let transactionId = getRollbackTransaction.txn_type === '2' ? getRollbackTransaction.txn_id : generateUUIDv7();
-                        data = {
-                            token, txn_id: transactionId, txn_ref_id, amount: rollbackAmount, description: `${rollbackAmount} Rollback for transaction with reference ID ${txn_ref_id}`, txn_type: 2
-                        }
-                    }
-                    const requestOptions = createOptions(operatorUrl, data);
+                    event === 'cashout' ? cashout_retries += 1 : rollback_retries += 1;
+                    let [getRollbackTransaction] = await write.query(`SELECT * FROM transaction WHERE txn_id = ?`, [txn_ref_id]);
+                    let txn_id = await generateUUIDv7();
+                    let amount = event == 'cashout' ? amount : getRollbackTransaction[0].amount;
+                    let description = event == 'cashout' ? description : `${amount} Rollback-ed for transaction with reference ID ${txn_ref_id}`;
+                    let txn_type = event == 'cashout' ? 1 : 2;
+                    let webhookData = { txn_id, amount, txn_ref_id, description, txn_type, token: options.token };
+                    const requestOptions = createOptions(operatorUrl, webhookData);
                     requestOptions.data.data = await encryption(requestOptions.data.data, secret);
                     await axios(requestOptions).then(async (data) => {
                         if (data.status == 200) {
-                            await write.query(`UPDATE pending_transactions SET txn_status = '2' where id = ?`, [id]);
-                            if (flag == 1) {
-                                await write.query(`INSERT INTO transaction (user_id, session_token , operator_id, txn_id, amount,  txn_ref_id , description, txn_type, txn_status) values (?,?,?,?,?,?,?,?,?)`, [validateUser.userId, data.token, operator_id, data.txn_id, data.amount, data.txn_ref_id, data.description, data.txn_type, '2']);
-                            } else {
+                            if(event === "cashout"){
                                 await write.query(`UPDATE transaction SET txn_status = '2' where id = ?`, [transaction_id]);
+                                await write.query(`UPDATE pending_transactions SET event = 'cashout', txn_status = '2' where id = ?`, [id]);
+                            }else{
+                                await write.query(`INSERT IGNORE INTO transaction (user_id, session_token , operator_id, txn_id, amount,  txn_ref_id , description, txn_type, txn_status) VALUES (?)`, [[]]);
+                                await write.query(`UPDATE transaction SET txn_status = '0' where id = ?`, [transaction_id]);
+                                await write.query(`UPDATE pending_transactions SET event = 'rollback', txn_status = '2' where id = ?`, [id]);
                             }
+                        } else {
+                            return res.status(400).send({ status: false, msg: `Request failed from upstream server` })
                         }
                     }).catch(async (err) => {
-                        if (cashout_retries == 10 && rollback_retries == 10) {
-                            await write.query(`UPDATE pending_transactions SET txn_status = '0' where id = ?`, [id]);
-                        }
-
                         return res.status(400).send({ status: false, msg: `Request failed from upstream server` })
                     });
+                    // if (event === 'cashout') {
+                    //     cashout_retries += 1;
+                    //     await write.query(`UPDATE pending_transactions SET cashout_retries = ?, event = ? WHERE id = ?`, [cashout_retries, 'cashout', id]);
+                    // } 
+                    // else {
+                    //     rollback_retries += 1;
+                    //     await write.query(`UPDATE pending_transactions SET rollback_retries = ?, event = ? WHERE id = ?`, [rollback_retries, 'rollback', id]);
+                    //     let { token, txn_ref_id } = options;
+                    //     let [getRollbackTransaction] = await write.query(`SELECT * FROM transaction WHERE txn_id = ?`, [txn_ref_id]);
+                    //     if (getRollbackTransaction.length > 1) {
+                    //         getRollbackTransaction = getRollbackTransaction[1];
+                    //     } else {
+                    //         getRollbackTransaction = getRollbackTransaction[0];
+                    //         rollbackFlag = 1;
+                    //     }
+                    //     let rollbackAmount = getRollbackTransaction.amount;
+                    //     let transactionId = getRollbackTransaction.txn_type === '2' ? getRollbackTransaction.txn_id : generateUUIDv7();
+                    //     data = {
+                    //         token, txn_id: transactionId, txn_ref_id, amount: rollbackAmount, description: `${rollbackAmount} Rollback for transaction with reference ID ${txn_ref_id}`, txn_type: 2
+                    //     }
+                    // }
+                    // operator
                 } else {
                     return res.status(400).send({ status: false, msg: "No URL configured for the event" });
                 }
@@ -91,6 +99,12 @@ const manualCashoutOrRollback = async (req, res) => {
     }
 }
 
+
+
+// Operator Rollback
+
+
+//Operator
 const operatorRollback = async (req, res) => {
     try {
         const { token } = req.headers;
@@ -110,7 +124,7 @@ const operatorRollback = async (req, res) => {
                     let rollbackAmount = getRollbackTransaction[0].amount;
                     const transactionId = await generateUUIDv7();
                     let data = {
-                         txn_id: transactionId, txn_ref_id, amount: rollbackAmount, description: `${rollbackAmount} Rollback-ed for transaction with reference ID ${txn_ref_id}`, txn_type: 2
+                        txn_id: transactionId, txn_ref_id, amount: rollbackAmount, description: `${rollbackAmount} Rollback-ed for transaction with reference ID ${txn_ref_id}`, txn_type: 2
                     };
                     const encryptedData = await encryption(data, secret);
                     const options = {
@@ -129,13 +143,13 @@ const operatorRollback = async (req, res) => {
                             const updatePendingTransaction = write.query(`UPDATE pending_transactions SET event = 'rollback', txn_status = '2' WHERE transaction_id = ?`, [getPendingTransaction[0].transaction_id]);
                             const updateTransaction = write.query(`UPDATE transaction SET txn_status = '0' where id = ?`, [getPendingTransaction[0].transaction_id]);
                             await Promise.all([insertTransaction, updatePendingTransaction, updateTransaction]);
-                            return res.status(200).send({ status: true, msg: `Bet rollback-ed successfully for reference ID ${txn_ref_id}`, data: encryptedData});
+                            return res.status(200).send({ status: true, msg: `Bet rollback-ed successfully for reference ID ${txn_ref_id}`, data: encryptedData });
                         } else {
                             console.log(`received an invalid response from upstream server`);
                             return res.status(data.status).send({ status: false, msg: `Request failed from upstream server with response:: ${JSON.stringify(data)}` })
                         }
                     }).catch(async err => {
-                        return res.status(500).send({ status: false, msg: "Request failed from upstream server", err});
+                        return res.status(500).send({ status: false, msg: "Request failed from upstream server", err });
                     })
                 } else {
                     return res.status(400).send({ status: false, msg: "No Pending Credits for this Reference ID" });
